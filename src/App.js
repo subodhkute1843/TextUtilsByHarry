9bf4a7657cd109bdfd8271c3cdfc2171d9f32c10
Subodh Kute <subodhkute07@gmail.com>
2023-10-31 13:27:42 +0530

    added toggle button to navbar for dark mod

--- src/App.js
+++ src/App.js
@@ -3,12 +3,19 @@ import './App.css';
 import Navbar from './Components/Navbar';
 import TextForm from './Components/TextForm';
 import About from './Components/About';
+import React, {useState} from 'react'
 
 function App() {
+  const [mode, setMode] = useState('light') //wheather dark mode is enabled or not 
 
+  const toggleMode = () => {
+    const newMode = mode === 'light' ? 'dark' : 'light';
+    setMode(newMode);
+  }
+  
   return (
     <>
-      <Navbar title="TextUtils" aboutText="About us" />
+      <Navbar title="TextUtils" aboutText="About us" mode = {mode} toggleMode={toggleMode}/>
       <div className="container my-3">
         {/* <TextForm heading = "enter the text to analyze"/> */}
       </div>
